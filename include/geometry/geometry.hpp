75840c5a6437136304a69f1ba4812e20d1a82a0c
SonaBadalyan <sona.badalyan.1992@gmail.com>
2024-08-13 19:29:51 +0400

    Added Point and Shape classes

--- include/geometry/geometry.hpp
+++ include/geometry/geometry.hpp
@@ -1,9 +1,26 @@
 // include/geometry/geometry.h
 #ifndef GEOMETRY_H
 #define GEOMETRY_H
+#include <vector>
 
-namespace geometry {
+namespace ODAGeometry {
 
+class Point {
+public:
+    double x, y;
+    Point(double x = 0, double y = 0) : x(x), y(y) {}
+    friend std::ostream& operator<<(std::ostream& os, const Point& p);
+    friend std::istream& operator>>(std::istream& is, Point& p);
+};
+
+class Shape {
+public:
+    virtual bool containsPoint(const Point& p) const = 0;
+    virtual std::vector<Point> getDrawSegments() const = 0;
+    virtual void serialize(std::ostream& os) const = 0;
+    virtual void deserialize(std::istream& is) = 0;
+    virtual ~Shape() = default;
+};
 
 } // namespace geometry
 
--- src/geometry.cpp
+++ src/geometry.cpp
@@ -1,7 +1,16 @@
 // src/geometry.cpp
 #include "geometry/geometry.h"
 
-namespace geometry {
+namespace ODAGeometry {
 
+std::ostream& Point::operator<<(std::ostream& os, const Point& p) {
+    os << p.x << " " << p.y;
+    return os;
+}
+
+std::istream& Point::operator>>(std::istream& is, Point& p) {
+    is >> p.x >> p.y;
+    return is;
+}
 
 } // namespace geometry
